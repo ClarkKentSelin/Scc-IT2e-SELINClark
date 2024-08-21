a8456e8c92d3c5a6801eb62b5b4984ef0389f5d6
ClarkKentSelin <marizrequinto@gmail.com>
2024-08-21 17:44:55 +0800

    first repo

---
+++ src/selin2eit/Selin2eit.java
@@ -0,0 +1,14 @@
+
+package selin2eit;
+
+public class Selin2eit {
+
+   
+    public static void main(String[] args) {
+        
+        Grades gr = new Grades();
+        gr.addGrades (1011, "Clark Pogi", 1.0, 2.2, 2.3, 1.3);
+        gr.viewGrades();
+    }
+    
+}
